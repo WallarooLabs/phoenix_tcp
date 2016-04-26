62ca2b98a05e9a4fbd9f17278c5468fd2ad70896
JONBRWN <jonathanbrown.a@gmail.com>
2016-04-26 16:07:56 -0400

    add supervisor

---
+++ lib/phoenix_tcp/supervisor.ex
@@ -0,0 +1,41 @@
+defmodule PhoenixTCP.Supervisor do
+	# the supervisor for the underlying handlers
+	@moduledoc false
+
+	use Supervisor
+	require Logger
+
+	def start_link(otp_app, endpoint, opts \\ []) do
+		Supervisor.start_link(__MODULE__, {otp_app, endpoint}, opts)
+	end
+
+	def init({otp_app, endpoint}) do
+		children = []
+
+		handler = endpoint.config(:tcp_handler)
+
+		if config = endpoint.config(:tcp) do
+			config = default(config, otp_app, 5001)
+			children = [handler.child_spec(:tcp, endpoint, config) | children]
+		end
+
+		supervise(children, strategy: :one_for_one)
+	end
+
+	defp default(config, otp_app, port) do
+		config =
+			config
+			|> Keyword.put_new(:otp_app, otp_app)
+			|> Keyword.put_new(:port, port)
+
+		Keyword.put(config, :port, to_port(config[:port]))
+	end
+
+	defp to_port(nil) do
+		Logger.error "TCP Server will not start because :port in config is nil, please use a valid port number"
+		exit(:shutdown)
+	end
+	defp to_port(binary)  when is_binary(binary),   do: String.to_integer(binary)
+	defp to_port(integer) when is_integer(integer), do: integer
+	defp to_port({:system, env_var}), do: to_port(System.get_env(env_var))
+end
