428891470d9a8013de928b9f89c795948f88d1c7
JONBRWN <jonathanbrown.a@gmail.com>
2017-09-13 22:42:41 -0400

    Updates RanchServer to use a Protocol Serializer on connect
    In order to generalize the RanchServer, it has been updated to accept a
    serializer via config options. If one is not provided, it defaults to using
    Poison. This is needed in order to parse the path and select a handler which
    will then use the serializer provided by the Transport for all other messages.

--- lib/phoenix_tcp/ranch_server.ex
+++ lib/phoenix_tcp/ranch_server.ex
@@ -1,7 +1,6 @@
 defmodule PhoenixTCP.RanchServer do
 	use GenServer
   require Logger
-  require IEx
 
   @behavious :ranch_protocol
 
@@ -16,58 +15,65 @@ defmodule PhoenixTCP.RanchServer do
     state = %{
       tcp_transport: tcp_transport,
       tcp_socket: tcp_socket,
-      handlers: Keyword.fetch!(opts, :handlers)
+      handlers: Keyword.fetch!(opts, :handlers),
+      serializer: opts[:serializer] || Poison
     }
     :gen_server.enter_loop(__MODULE__, [], state)
   end
 
-  def handle_info({:tcp, tcp_socket, data}, %{handlers: handlers, tcp_transport: tcp_transport} = state) do
-    %{"path" => path, "params" => params} =
-      String.rstrip(data)
-      |> Poison.decode!()
-    case Map.get(handlers, path) do
-      # handler is the server which handles the tcp messages
-      # currently there is only one server, 
-      # module is the transport module
-      # opts = {endpoint, socket, transport}
-      # endpoint being the endpoint defined in the phx app
-      # socket being the socket defined in the phx app
-      # transport being the atom defining the transport
-      {handler, module, opts} ->
-        case module.init(params, opts) do
-          {:ok, {module, {opts, timeout}}} ->
-            state = %{
-              tcp_transport: tcp_transport,
-              tcp_socket: tcp_socket,
-              handler: handler,
-              transport_module: module,
-              transport_config: opts,
-              timeout: timeout
-            }
-            :ok = tcp_transport.setopts(tcp_socket, [active: :once])
-            connected_msg = Poison.encode!(%{"payload" => %{"status" => "ok", "response" => "connected"}})
-            tcp_transport.send(tcp_socket, connected_msg)
-            {:noreply, state, timeout}
-          {:error, error_msg} ->
-            status_error_msg = Poison.encode!(%{"payload" => %{"status" => "error", "response" => error_msg}})
-            tcp_transport.send(tcp_socket, status_error_msg)
+  def handle_info({:tcp, tcp_socket, data}, %{handlers: handlers, tcp_transport: tcp_transport, serializer: serializer} = state) do
+    case serializer.decode(data) do
+      {:ok, %{"path" => path, "params" => params}} ->
+        case Map.get(handlers, path) do
+          # handler is the server which handles the tcp messages
+          # currently there is only one server,
+          # module is the transport module
+          # opts = {endpoint, socket, transport}
+          # endpoint being the endpoint defined in the phx app
+          # socket being the socket defined in the phx app
+          # transport being the atom defining the transport
+          {handler, module, opts} ->
+            case module.init(params, opts) do
+              {:ok, {module, {opts, timeout}}} ->
+                state = %{
+                  tcp_transport: tcp_transport,
+                  tcp_socket: tcp_socket,
+                  handler: handler,
+                  transport_module: module,
+                  transport_config: opts,
+                  timeout: timeout
+                }
+                :ok = tcp_transport.setopts(tcp_socket, [active: :once])
+                connected_msg = serializer.encode!(connected_json)
+                tcp_transport.send(tcp_socket, connected_msg)
+                {:noreply, state, timeout}
+              {:error, error_msg} ->
+                status_error_msg = serializer.encode!(%{"payload" => %{"status" => "error", "response" => error_msg}})
+                tcp_transport.send(tcp_socket, status_error_msg)
+                :ok = tcp_transport.setopts(tcp_socket, [active: :once])
+                {:noreply, state}
+            end
+          nil ->
+            error_msg = serializer.encode!(%{"payload" => %{"status" => "error", "response" => "no path matches"}})
+            tcp_transport.send(tcp_socket, error_msg)
             :ok = tcp_transport.setopts(tcp_socket, [active: :once])
             {:noreply, state}
         end
-      nil ->
-        error_msg = Poison.encode!(%{"payload" => %{"status" => "error", "response" => "no path matches"}})
-        tcp_transport.send(tcp_socket, "no path matches")
+      {:error, _error} ->
+        error_msg = serializer.encode!(%{"payload" => %{"status" => "error", "response" => "Unable to decode data with #{serializer}"}})
+        tcp_transport.send(tcp_socket, error_msg)
+        Logger.warn "Unable to decode data in #{__MODULE__}'s handle_info() using #{serializer}"
         :ok = tcp_transport.setopts(tcp_socket, [active: :once])
         {:noreply, state}
     end
   end
 
-  def handle_info({:tcp, _tcp_socket, payload}, 
+  def handle_info({:tcp, _tcp_socket, payload},
     %{transport_module: module, transport_config: config} = state) do
     handle_reply state, module.tcp_handle(payload, config)
   end
 
-  def handle_info({:tcp_closed, _tcp_socket}, 
+  def handle_info({:tcp_closed, _tcp_socket},
     %{transport_module: module, transport_config: config} = state) do
     module.tcp_close(config)
     {:stop, :shutdown, state}
@@ -104,12 +110,20 @@ defmodule PhoenixTCP.RanchServer do
     {:noreply, new_state, timeout}
   end
 
-  defp handle_reply(%{timeout: timeout, tcp_transport: transport, tcp_socket: socket} = state, 
+  defp handle_reply(%{timeout: timeout, tcp_transport: transport, tcp_socket: socket} = state,
       {:reply, {_encoding, encoded_payload}, new_config}) do
     transport.send(socket, encoded_payload)
     :ok = transport.setopts(socket, [active: :once])
     new_state = Map.put(state, :transport_config, new_config)
     {:noreply, new_state, timeout}
+  end
+
+  defp connected_json do
+    %{"payload" => %{"status" => "ok",
+      "response" => "connected"},
+    "topic" => "",
+    "event" => "",
+    "ref" => nil}
   end
 
 end
