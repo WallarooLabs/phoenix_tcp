016200fc8946261cb85ea1439e64e1a6b727beeb
JONBRWN <jonathanbrown.a@gmail.com>
2017-09-15 13:06:08 -0400

    Make the tcp_server used by the Ranch Adapter configurable
    The RanchAdapter was hardcoded to always use the RanchServer. This
    change allows a user to pass :tcp_server to the :tcp config and be able
    to set their own server if they choose to do so. Defaults to
    RanchServer if one is not provided.

--- lib/phoenix_tcp/adapters/ranch.ex
+++ lib/phoenix_tcp/adapters/ranch.ex
@@ -1,10 +1,10 @@
 defmodule PhoenixTCP.Adapters.Ranch do
-	
-	def args(scheme, plug, _opts, ranch_options) do
-		ranch_options
-			|> Keyword.put_new(:ref, build_ref(plug, scheme))
-			|> to_args()
-	end
+
+  def args(scheme, plug, _opts, ranch_options) do
+    ranch_options
+      |> Keyword.put_new(:ref, build_ref(plug, scheme))
+      |> to_args()
+  end
 
   @doc """
   Shutdowns the given reference.
@@ -17,12 +17,12 @@ defmodule PhoenixTCP.Adapters.Ranch do
   Returns a child spec to be supervised by your application.
   """
   def child_spec(scheme, plug, opts, ranch_options \\ []) do
-    [ref, nb_acceptors, trans_opts, proto_opts] = args(scheme, plug, opts, ranch_options)
+    [ref, nb_acceptors, tcp_server, trans_opts, proto_opts] = args(scheme, plug, opts, ranch_options)
     ranch_module = case scheme do
       :tcp -> :ranch_tcp
       # add ssl later?
     end
-    :ranch.child_spec(ref, nb_acceptors, ranch_module, trans_opts, PhoenixTCP.RanchServer, proto_opts)
+    :ranch.child_spec(ref, nb_acceptors, ranch_module, trans_opts, tcp_server, proto_opts)
   end
 
   @tcp_ranch_options [port: 5001]
@@ -34,11 +34,11 @@ defmodule PhoenixTCP.Adapters.Ranch do
     {ref, opts} = Keyword.pop(opts, :ref)
     {handlers, opts} = Keyword.pop(opts, :handlers)
     {acceptors, opts} = Keyword.pop(opts, :acceptors, 100)
+    {tcp_server, opts} = Keyword.pop(opts, :tcp_server, PhoenixTCP.RanchServer)
     {protocol_options, opts} = Keyword.pop(opts, :protocol_options, [])
-
     {extra_options, transport_options} = Keyword.split(opts, @protocol_options)
     protocol_options = [handlers: handlers] ++ protocol_options ++ extra_options
-    [ref, acceptors, initial_transport_options ++ transport_options, protocol_options]
+    [ref, acceptors, tcp_server, initial_transport_options ++ transport_options, protocol_options]
   end
 
   defp build_ref(plug, scheme) do
